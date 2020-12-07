6155cc6d63c2bf1057533a6e8714d682e2fe54ed
Rafael Mendes <rafael.mendes1101@gmail.com>
2020-12-07 16:57:27 +0000

    Created api routes files

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-// @route  GET api/profile
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+// @route  GET api/profile/me
 // @desc   Test route
 // @access Public
-router.get('/', (req, res) => {
-	res.send('Profile Route');
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
